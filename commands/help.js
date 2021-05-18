9ea90018c7168de69c3783a3a5ad3b6a2f79410b
harshpac <65284517+harshpac@users.noreply.github.com>
2021-05-19 00:05:39 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -17,96 +17,114 @@ module.exports = {
             {
                name: "**`prefix`**",
                value: 'I will tell you the current prefix',
-               inline: false
+               inline: true
             }, 
 
            {
                name: "**`setagenda <..>`**",
                value: 'I will set the agenda of the chat ',
-               inline: false
+               inline: true
            },
 
            {
             name: "**`agenda`**",
             value: 'I will tell you the current agenda  ',
-            inline: false
+            inline: true
            },
 
            {
             name: "**`setlink <..>`**",
             value: 'I will set the link for the chat ',
-            inline: false
+            inline: true
            },
 
            {
             name: "**`link`**",
             value: 'I will provide the link ',
-            inline: false
+            inline: true
            },
            
            {
                name: "**`change <..>`**",
                value: 'I will change the prefix',
-               inline: false
+               inline: true
            },
 
            {
                name: "**`joke`**",
                value: 'I will crack a joke for you',
-               inline: false
+               inline: true
            },
 
            {
             name: "**`clear <number>`**",
             value: 'I will delete <number> recent messages (ADMIN ONLY) ',
-            inline: false
+            inline: true
            },
 
            {
             name: "**`clear all`**",
             value: 'I will delete messages in bulk (ADMIN ONLY)',
-            inline: false
+            inline: true
            },
          
            {
             name: "**`spam <..>`**",
             value: 'I will start spamming until someone asks me to stop',
-            inline: false
+            inline: true
            },
 
            {
             name: "**`stop`**",
             value: 'I will stop spamming ',
-            inline: false
+            inline: true
            },
 
            {
                name: "**`allow all`**",
-               value: 'Allow group members to delete messages'
+               value: 'Allow group members to delete messages',
+               inline: true
            },
 
            {
             name: "**`allow admin`**",
-            value: 'Allow only admin to delete messages'
+            value: 'Allow only admin to delete messages',
+            inline: true
            },
 
            {
             name: "**`tea <tag>`**",
-            value: 'You can provide a cup of tea to the tagged user (Just for fun)'
+            value: 'You can provide a cup of tea to the tagged user (Just for fun)',
+            inline: true
            },
 
            {
             name: "**`tea forme`**",
-            value: 'Check how many cups of tea you have been provided by your friends'
+            value: 'Check how many cups of tea you have been provided by your friends',
+            inline: true
            },
            {
             name: "**`google <query>`**",
-            value: 'I will google a query for you'
+            value: 'I will google a query for you',
+            inline: true
            },
 
            {
             name: "**`typing <tag_user1> <tag_user2> ...`** ",
-            value: 'I will start a 2 min typing speed test '
+            value: 'I will start a 2 min typing speed test ',
+            inline: true
+           },
+
+           {
+            name: "**`contest`**",
+            value: 'I will show the list of upcoming CP contests ',
+            inline: true
+           }, 
+
+           {
+            name: "**`contest <sitename>`**",
+            value: 'I will show the Contest for particular site',
+            inline: true
            }
 
        );
